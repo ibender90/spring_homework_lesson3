a9b30d859451ef907b3b65bdc2ccf880f444c429
Artjom_Stalev <stalev.artjom@gmail.com>
2022-12-07 10:40:08 +0200

    git init

---
+++ src/main/java/com/geek/lesson3homework/Lesson3homeworkApplication.java
@@ -0,0 +1,13 @@
+package com.geek.lesson3homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson3homeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson3homeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geek/lesson3homework/Lesson3homeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.lesson3homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson3homeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
